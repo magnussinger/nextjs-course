90482332f4d9d8b2fb9b768a31fc29dd0d4bc57c
Magnus Singer <work@magnus-singer.com>
2024-01-20 22:26:23 +0100

    fix auth.js

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
-import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import { authConfig } from './auth.config';
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
@@ -26,10 +26,12 @@ export const { auth, signIn, signOut } = NextAuth({
                     .safeParse(credentials);
 
                 if (parsedCredentials.success) {
-                    const {email, password} = parsedCredentials.data;
-                    const user = await getUser(email);
-                    const passwordsMatch = await bcrypt.compare(password, user.password);
+                    const { email, password } = parsedCredentials.data;
 
+                    const user = await getUser(email);
+                    if (!user) return null;
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
                     if (passwordsMatch) return user;
                 }
 
