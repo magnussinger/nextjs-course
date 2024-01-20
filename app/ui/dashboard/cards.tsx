bb76e43efaca41c5de8aee2feccc3fd2cddf5be5
Magnus Singer <work@magnus-singer.com>
2024-01-20 22:19:51 +0100

    finish course

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,25 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+import { fetchCardData } from '@/app/lib/data';
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <><Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
